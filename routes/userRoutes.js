ec8bdbd0f6f3e54863afe84626315de9ee344b12
Gustavhillmann2 <guhi24ab@student.cbs.dk>
2025-11-26 16:20:14 +0100

    Validerer login funktion

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/userController');
 
 // This route listens for POST requests at /api/users
 router.post('/users', userController.createUser);
+router.post('/login', userController.loginUser);
 
 module.exports = router;
