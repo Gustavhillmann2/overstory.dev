bda0e8bf58f44b0237e5b61bb01f2bc07cc278e8
Alexander Lorentzen Villekjaer <bungushead1@gmail.com>
2025-11-26 12:53:10 +0100

    Ændring i sendSms.js

--- models/sendSms.js
+++ models/sendSms.js
@@ -7,17 +7,16 @@ const authToken  = process.env.TWILIO_AUTH_TOKEN;
 
 const client = twilio(accountSid, authToken);
 
-async function sendSms() {
+async function sendSms(to,from,body) {
   try {
     const message = await client.messages.create({
-      body: 'Du kan vinde millioner',
-      from: '+19786072987',
-      to: '+4529878824'
+      body: body,
+      from: from,
+      to: to
     });
     console.log('Message sent with SID:', message.sid);
   } catch (error) {
     console.error('Error sending SMS:', error);
   }
 }
-
-sendSms();
+sendSms('+123','+123','Lille besked')
