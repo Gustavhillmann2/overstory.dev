8c8e88fb285a42ad4d21263640bcc0df3b86e06c
Gustavhillmann2 <guhi24ab@student.cbs.dk>
2025-11-26 13:54:02 +0100

    Tilføjet phone til databasen

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ const userRoutes = require('./routes/userRoutes');
 app.use("/api", userRoutes);
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
+app.get("/events", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "event.html"));
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
