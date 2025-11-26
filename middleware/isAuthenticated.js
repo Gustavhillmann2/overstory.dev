deddf370f14fdf081915e09ad8b0fbbb33cbec98
Alexander Lorentzen Villekjaer <bungushead1@gmail.com>
2025-11-26 14:06:38 +0100

    middleware

--- middleware/isAuthenticated.js
+++
@@ -1,9 +0,0 @@
-/* OBS her et udkast til hvordan vores middleware kan se ud med at tjekke 
-om man er logget ind (taget fra aktieporteføljekoden) - fjern kommentering
-// Function til at tjekke om brugeren er logget ind
-export function isAuthenticated(req, res, next) {
-    if (!req.session || !req.session.user) {
-        return res.redirect('/login');
-    }
-    next(); // Giv kontrollen videre til næste funktion (fx controlleren)
-} */
