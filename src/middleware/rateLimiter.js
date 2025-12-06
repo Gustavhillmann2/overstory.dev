7f49d19c3679241fae0f41046cc4310478329b4f
Gustavhillmann2 <guhi24ab@student.cbs.dk>
2025-12-06 11:13:12 +0100

    Tilføjet respons tid middleware til at vise resposn tid på http requests

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -3,7 +3,8 @@ const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // definere hvor længe en rate-limit periode varer
     max: 100, // limiter hver IP to 100 requests pr. windowMs
-    message: 'Too many requests from this IP, please try again later.'
+    message: 'Too many requests from this IP, please try again later.',
+    statusCode: 429, // HTTP status kode for "Too Many Requests"
 })
 
 module.exports = limiter; // eksportere rate-limiter middleware
