35c04a906bf98affd1a3d2713f7aa0615ef3dc60
Alexander Lorentzen Villekjaer <bungushead1@gmail.com>
2025-12-09 16:25:03 +0100

    Rykket lidt rundt + cors(begrænser domæner) og morgan(logger alle HTTP request)

--- src/app.js
+++ src/app.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const path = require("path");
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const csrf = require('csurf');
 const { limiter } = require('./middleware/rateLimiter');
 const sessionMiddleware = require('./middleware/sessionMiddleware');
 const responsTimeMiddleware = require ('./middleware/responseTime');
-const helmet = require('helmet');
-const csrf = require('csurf');
-
 const app = express();
+
+// Morgan logger alle HTTP requests til konsollen
+if (process.env.NODE_ENV === 'production') {
+  app.use(morgan('combined'));
+} else {
+  app.use(morgan('dev'));
+}
+
+// CORS begrænser hvilke domæner der kan tilgå applikationen
+app.use(cors({
+  origin: ['https://overstory.dk'], // Tillad kun anmodninger fra denne oprindelse
+  methods: ['GET', 'POST'], // Tillad kun disse HTTP metoder
+  credentials: true // Tillad cookies og autorisations headers
+}));
+
+// endpoint beskyttelse med helmet middleware (Sætter sikre HTTP headers)
+app.use(helmet());
 
 // Håndterer form data og JSON
 app.use(express.urlencoded({ extended: true }));
@@ -18,16 +36,15 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(sessionMiddleware); // Anvender session middleware
 app.use(csrfProtection); // Anvender CSRF beskyttelse middleware
-app.use(limiter); // Anvender rate limiter middleware
-app.use(responsTimeMiddleware); // Avender response time middleware
+const csrfProtection = csrf(); // Initialiserer CSRF beskyttelse middleware
 
-// CSRF beskyttelse
-const csrfProtection = csrf();
-// Make token available in all views
+// gør CSRF token tilgængelig i alle views
 app.use((req, res, next) => {
     res.locals.csrfToken = req.csrfToken();
     next();
 });
+
+// Håndterer CSRF fejl
 app.use((err, req, res, next) => {
   if (err.code === 'EBADCSRFTOKEN') {
       return res.status(403).send('Form tampered with.');
@@ -35,8 +52,8 @@ app.use((err, req, res, next) => {
   next(err);
 });
 
-// endpoint beskyttelse med helmet middleware
-app.use(helmet());
+app.use(limiter); // Anvender rate limiter middleware
+app.use(responsTimeMiddleware); // Avender response time middleware
 
 // Serverer statiske filer (css, osv)
 app.use(express.static(path.join(__dirname, 'public')));
