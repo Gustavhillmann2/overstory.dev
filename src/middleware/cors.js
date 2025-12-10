fdd723baf710f897f3d5670b6f846c7645661c95
Gustavhillmann2 <guhi24ab@student.cbs.dk>
2025-12-10 18:32:40 +0100

    Rodet op i app.js og rykket logik til seperate filer

---
+++ src/middleware/cors.js
@@ -0,0 +1,15 @@
+const cors = require('cors'); // Importerer cors biblioteket
+
+// Henter CORS_ORIGIN miljøvariablen, falder tilbage til standard URL.
+const CORS_ORIGIN = process.env.CORS_ORIGIN || 'https://overstory.dk';
+
+const corsMiddleware = cors({
+  origin: CORS_ORIGIN, // Tillader forespørgsler fra den definerede origin
+  
+  methods: ['GET', 'POST', 'OPTIONS'], // Tillader kun disse HTTP-metoder
+  
+  // Tillader overførsel af credentials (cookies/sessions-id). Dette er nødvendigt, da appen bruger express-session.
+  credentials: true,
+});
+
+module.exports = corsMiddleware;
