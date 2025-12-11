f7f1daccd802099d8a3cfa0bd7acf4380ed66cbd
Gustavhillmann2 <guhi24ab@student.cbs.dk>
2025-12-11 11:43:27 +0100

    Fikset helmet som blokere cloudinary

--- src/middleware/helmetMiddleware.js
+++ src/middleware/helmetMiddleware.js
@@ -6,7 +6,7 @@ const helmetMiddleware = helmet({
           defaultSrc: ["'self'"], // Kommentar
           scriptSrc: ["'self'"], // Kommentar
           connectSrc: ["'self'"],
-          styleSrc: ["'self'", "https://fonts.googleapis.com"],
+          styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
           fontSrc: ["'self'", "https://fonts.gstatic.com"],
           imgSrc: ["'self'", "https://res.cloudinary.com"],
           objectSrc: ["'none'"],
