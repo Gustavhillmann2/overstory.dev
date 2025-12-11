64e60e78267dff64cc858d81ed4fa5fde54f71c1
Gustavhillmann2 <guhi24ab@student.cbs.dk>
2025-12-11 23:36:08 +0100

    Tilføjet kommentarer til smsService.js

--- src/services/smsService.js
+++ src/services/smsService.js
@@ -1,24 +1,26 @@
-require('dotenv').config();
-const twilio = require('twilio');
+require('dotenv').config(); // Henter variabler fra .env
+const twilio = require('twilio'); // Importerer Twilio biblioteket
 
-const ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
-const AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+const ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID; // Henter twilio konto SID fra .env
+const AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN; // Henter twilio auth token fra .env
 
-const client = twilio(ACCOUNT_SID, AUTH_TOKEN);
+const client = twilio(ACCOUNT_SID, AUTH_TOKEN); // Opretter Twilio klienten
 
+// Funktion til at sende SMS ved event registrering
 async function sendEventRegistrationSms(to, event) {
 	try {
+		// Sender SMS via Twilio klienten
 		const message = await client.messages.create({
 			body: `Hi! You are now signed up for "${event.title}" d. ${event.date}. See you there!`,
 			from: process.env.TWILIO_PHONE,
 			to: to
 		});
 
-		console.log("SMS sent:", message.sid);
+		console.log("SMS sent:", message.sid); // Log besked SID ved succes
 	} catch (err) {
-		console.error("Twilio SMS error:", err);
-		throw err;
+		console.error("Twilio SMS error:", err); // Log fejl til konsollen
+		throw err; // Kaster fejlen så den kan håndteres i controlleren
 	};
 };
 
-module.exports = { sendEventRegistrationSms };	
+module.exports = { sendEventRegistrationSms }; // Eksporterer funktionen til brug i controlleren
